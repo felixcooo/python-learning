1e5003be217f918d1225379fb90a83b02a2d6ad3
felixcooo <felixmiguelcarreras@gmail.com>
2026-04-28 20:44:16 -0400

    Topic 2: Variables and data types - str, int, float, bool

---
+++ my_profile.py
@@ -0,0 +1,14 @@
+name = "Felix"
+age = 33
+height = 1.82
+using_raspberry_pi = True
+
+print("Name: ", name)
+print("Age: ", age)
+print("Height: ", height)
+print("Using Rasberry Pi:", using_raspberry_pi)
+print()
+print("Type of name: ", type(name))
+print("Type of age: ", type(age))
+print("Type of height: ", type(height))
+print("Type of using Rasberry Pi:", type(using_raspberry_pi))
