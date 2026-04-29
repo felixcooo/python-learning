cbe7ff04b5aef05aa9a9f3f5770c30013d6ff1cd
felixcooo <felixmiguelcarreras@gmail.com>
2026-04-28 20:47:32 -0400

    Topic 2: Variables and data types - str, int, float, bool

---
+++ data_type.py
@@ -0,0 +1,15 @@
+first_name = "Alice"
+last_name = "Rasbberry"
+greeting = "Hello, World!"
+age = 25
+year = 2026
+temperature = -5
+score = 0
+
+print(first_name)
+print(last_name)
+print(greeting)
+print(age)
+print(year)
+print(temperature)
+print(score)
